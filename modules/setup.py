a62eb72cdb8899e7f6e15541c0d0f0dd062bb24d
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-06-06 16:15:11 +0300

    Distutils

---
+++ modules/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name = 'modules',
+      version = '1.0',
+      py_modules = ['__init__', 'apartment', 'arrays', 'cars','jobs', 'platform_element', 'read_states',
+                    'start_read_file', 'take_data']
+
+)
